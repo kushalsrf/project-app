8ddd46d7156ea86a9f5e975cbbfcf1e3c9e78e3b
kushalsrf <70914024+kushalsrf@users.noreply.github.com>
2021-05-26 16:07:29 +0530

    Update secondwallet.php

--- secondwallet.php
+++ secondwallet.php
@@ -8,17 +8,20 @@ if ($_SERVER["REQUEST_METHOD"] === 'GET')
      if(isset($_GET['user'])){
         $user = mysqli_real_escape_string($conn,$_GET['user']);
     }else{
-          echo "user not set in GET Method";
+        echo json_encode(array('status'=>'fail', 'message'=>'Please provide user'));
+
    }
     if(isset($_GET['amount'])){
         $amount = mysqli_real_escape_string($conn,$_GET['amount']);
      }else{
-        echo  "amount not set in GET Method";
+        echo json_encode(array('status'=>'fail', 'message'=>'Please provide amount'));
+
      }
      if(isset($_GET['orderid'])){
         $orderid = mysqli_real_escape_string($conn,$_GET['orderid']);
         }else{
-     echo   "orderid not set in GET Method";
+            echo json_encode(array('status'=>'fail', 'message'=>'Please provide orderid'));
+
      }
    
     $query = "INSERT INTO wallet(`user`,`amount`,`orderid`)   VALUES('$user','$amount','$orderid')";
