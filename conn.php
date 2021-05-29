f66933c0870ac33666403edc947e8d14bb6cf1bb
kushalsrf <70914024+kushalsrf@users.noreply.github.com>
2021-05-29 11:17:48 +0530

    Delete conn.php

--- conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost:3307", "root@", "", "ex");
- 
-// Check connection
-if($mysqli === false){
-    die("ERROR: Could not connect. " . $mysqli->connect_error);
-}
-?>
